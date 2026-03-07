90649277a8d59073acd2f522aa6044547d27a837
Frasier <fsundra@gmail.com>
2026-03-07 13:37:26 +0800

    adds: parsing .env file

--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"log"
+
+	"github.com/joho/godotenv"
+)
 
 
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Could not parse .env file %v", err)
+		panic(err)
+	}
+
 	var port int
 	flag.IntVar(&port, "port", 8800, "Starting port of the app")
 	flag.Parse()
