50f9661a3f3e3321381c16b1780717bb24401365
Frasier <fsundra@gmail.com>
2026-03-07 14:36:04 +0800

    adds: docker compose

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,14 +1,17 @@
 package app
 
 import (
+	"database/sql"
 	"os"
 
+	"github.com/Fozzyack/habit-tracker/internal/database"
 	"github.com/Fozzyack/habit-tracker/internal/env"
 	"github.com/rs/zerolog"
 )
 
 type Application struct {
 	Logger zerolog.Logger
+	DB     *sql.DB
 }
 
 func NewApplication() (*Application, error) {
@@ -17,8 +20,14 @@ func NewApplication() (*Application, error) {
 		logger = zerolog.New(zerolog.ConsoleWriter{Out: os.Stdout}).With().Timestamp().Logger()
 	}
 
+	pgDB, err := database.Open()
+	if err != nil {
+		return nil, err
+	}
+
 	app := &Application{
 		Logger: logger,
+		DB:     pgDB,
 	}
 
 	return app, nil
