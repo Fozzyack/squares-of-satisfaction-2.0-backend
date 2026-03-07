1ca7a373c4be30b95640301b64c09eb27b48fee3
Frasier <fsundra@gmail.com>
2026-03-07 14:57:17 +0800

    adds: migrations folder and auto db migrations

--- internal/database/database.go
+++ internal/database/database.go
@@ -2,9 +2,12 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
+	"io/fs"
 
 	"github.com/Fozzyack/habit-tracker/internal/env"
-	_"github.com/jackc/pgx/v5/stdlib"
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/pressly/goose/v3"
 )
 
 func Open() (*sql.DB, error) {
@@ -19,5 +22,30 @@ func Open() (*sql.DB, error) {
 	}
 
 	return db, nil
+
+}
+
+func MigrateFS(db *sql.DB, migrationFS fs.FS, dir string) error {
+	goose.SetBaseFS(migrationFS)
+	defer func() {
+		goose.SetBaseFS(nil)
+	}()
+	return Migrate(db, dir)
+
+}
+
+func Migrate(db *sql.DB, dir string) error {
+	err := goose.SetDialect("postgres")
+	if err != nil {
+		return fmt.Errorf("Goose Dialect set: %w", err)
+	}
+
+	err = goose.Up("postgres")
+	if err != nil {
+		return fmt.Errorf("Goose up: %w", err)
+	}
+	return nil
+
+
 
 }
---
+++ migrations/001_create_users.sql
@@ -0,0 +1,16 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE TABLE users (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    name VARCHAR(63) NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW()
+);
+-- +goose StatementEnd
+-- +goose Up
+-- +goose StatementBegin
+DROP TABLE users; 
+-- +goose StatementEnd
